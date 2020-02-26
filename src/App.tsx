131585ff310788813ba3301553571d273b7ab27d
Jonathan Tuzman <jtuzman@sunriselabs.com>
2020-02-26 15:34:16 -0500

    parse lines of text

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div id="container">
-      <div id="gutter"/>
-      <div id="editor"></div>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    text: 'one line of text \nanother line of text',
+  };
+
+  get lines(): string[] {
+    return this.state.text.split('\n');
+  }
+
+  changeText = (event: any) => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
+  render() {
+    return (
+      <div id="container">
+        <div id="editor-container">
+          <div id="gutter"/>
+          <div id="editor">
+          <textarea
+            value={ this.state.text }
+            onChange={ this.changeText }
+          />
+          </div>
+        </div>
+        <div id="other-stuff">
+          { this.lines.map(line =>
+            <p>{ line.length } - { line }</p>)
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
