8da5d3621ab233314d949f7fa3f59338664355b2
Jonathan Tuzman <jtuzman@sunriselabs.com>
2020-03-04 14:49:33 -0500

    refactor column separator

--- src/models/TextEntryModel.ts
+++ src/models/TextEntryModel.ts
@@ -1,7 +1,7 @@
 export class TextLine {
   text = '';
   indentLevel = 0;
-  
+
   constructor(text: string) {
     this.text = text;
   }
@@ -9,36 +9,26 @@ export class TextLine {
 
 export class TextEntryModel {
   lines: TextLine[] = [];
-  
+
   setText = (text: string) => {
     // break the string into lines
     const linesOfText = text.split('\n');
-    this.lines.length = 0;
-    linesOfText.forEach(text => {
-      // create the line
-      const line = new TextLine('');
-      const components = text.split(':');
-      // if no colon
-      if (components.length === 1) {
-        line.text = components[0];
-        line.indentLevel = 0;
-      } else {
-        line.text = components[1];
-        line.indentLevel = Number(components[0]);
-      }
-      // add it to our lines
-      this.lines.push(line);
-    });
-
-    /*
-        // todo trim whitespace at line end
-        this.lines = linesOfText.map(text => new TextLine(text));
-
-        // todo lines should be in the format {number}:{text} where number represents the indent level
-        // then we'll split on the colon and store the lines'
-    */
-
+    this.lines = linesOfText.map(text => this.getColonSeparatedIndentLevel(text));
   };
+
+  getColonSeparatedIndentLevel(text: string): TextLine {
+    const line = new TextLine('');
+    const components = text.split(':');
+    // if no colon
+    if (components.length === 1) {
+      line.text = components[0];
+      line.indentLevel = 0;
+    } else {
+      line.text = components[1];
+      line.indentLevel = Number(components[0]);
+    }
+    return line;
+  }
 
   getText = (): string => this.lines.map(l => l.text).join('\n');
 
