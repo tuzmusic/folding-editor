b359db40129dc42766f26b124d7808318acb6243
Jonathan Tuzman <jtuzman@sunriselabs.com>
2020-03-26 19:17:52 -0400

    Fix Button style; Fix selection on fold

--- src/components/DraftJS/ControlledEditor.tsx
+++ src/components/DraftJS/ControlledEditor.tsx
@@ -14,22 +14,25 @@ p content`;
 
 const startingContent = ContentState.createFromText(startText);
 
-const InvisibleBlock = styled.div({});
-const FoldButton = styled.div({
-  borderRadius: '50px',
-  background: 'lightblue',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  margin: '20px',
-  padding: '20px',
-  height: '50px',
-  width: '50px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+const StyleWrapper = styled.div({
+  '& .DraftEditor-root': {
+    padding: '0 20px',
+  },
 });
+const InvisibleBlock = styled.div({ display: 'none' });
+const FoldButton = styled.button(({
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    margin: '20px',
+  }),
+  ({ disabled }) => ({ opacity: disabled ? 0.5 : 1 }), // default disabled opacity isn't transparent enough
+);
 
+type FoldingInfo = {
+  parentKey: string,
+  childKeys: string[]
+}
 type Props = {};
 type State = {
   editorState: EditorState
@@ -49,32 +52,43 @@ class DJEditor extends React.Component<Props, State> {
 
   get foldedParentKeys(): string[] { return Object.keys(this.state.foldedParents); }
 
+  get currentFoldableNode(): FoldingInfo | null {
+    // only fold when there is no selection
+    // todo: it should actually be pretty easy to implement foldSelection()
+    if (!this.selection.isCollapsed()) return null;
+
+    const anchorKey = this.selection.getAnchorKey();
+    const parentNode = this.state.documentModel.getNodeByKey(anchorKey);
+
+    // only container nodes can be folded
+    if (!(parentNode instanceof DJContainerNode)) return null;
+
+    return {
+      parentKey: anchorKey,
+      childKeys: parentNode.getAllChildKeys(),
+    };
+  }
+
   render() {
     const { editorState } = this.state;
     const { updateState, blockRen, foldAtCursor } = this;
     this.bindToWindow(); // make lots of useful stuff accessible to the console
 
     return (
-      <>
-        <FoldButton onClick={ foldAtCursor }>
+      <StyleWrapper>
+        <FoldButton onClick={ foldAtCursor } disabled={ !this.currentFoldableNode }>
           Fold
         </FoldButton>
         <Editor
           editorState={ editorState }
-          wrapperClassName="wrapper"
-          editorClassName="editor"
+          wrapperClassName="dj-wrapper"
+          editorClassName="dj-editor"
           onEditorStateChange={ updateState }
           customBlockRenderFunc={ blockRen }
-        /></>
+        />
+      </StyleWrapper>
     );
   }
-
-  forceRender = () => {
-    const { editorState } = this.state;
-    const content = editorState.getCurrentContent();
-    const newEditorState = EditorState.createWithContent(content);
-    this.setState({ editorState: newEditorState });
-  };
 
   get rawContentState(): RawDraftContentState {
     return convertToRaw(this.state.editorState.getCurrentContent());
@@ -89,21 +103,24 @@ class DJEditor extends React.Component<Props, State> {
     this.setState({ editorState: newEditorState, documentModel: newModelState });
   };
 
+  forceRender = () => {
+    const { editorState } = this.state;
+    const currentSelection = this.selection;
+    const content = editorState.getCurrentContent();
+    const newEditorState = EditorState.createWithContent(content);
+    const stateWithSelection = EditorState.forceSelection(newEditorState, currentSelection);
+    this.setState({ editorState: stateWithSelection });
+  };
+
   foldAtCursor = () => {
-    // this function is for folding at a single cursor position
-    // todo: it should actually be pretty easy to implement foldSelection()
-    if (!this.selection.isCollapsed()) return;
-    const anchorKey = this.selection.getAnchorKey();
-    const node = this.state.documentModel.getNodeByKey(anchorKey);
-    if (!(node instanceof DJContainerNode)) return;
-
-    const parentNode = node as DJContainerNode;
-
-    this.setState(({ foldedParents }) => {
-      if (this.foldedParentKeys.includes(anchorKey)) { // UNFOLD
-        delete foldedParents[anchorKey];
+    const foldingInfo = this.currentFoldableNode;
+    if (!foldingInfo) return;
+    const { parentKey, childKeys } = foldingInfo;
+    this.setState(({ foldedParents }) => { // we're mutating foldedParents so we need to operate in a setState callback
+      if (this.foldedParentKeys.includes(parentKey)) { // UNFOLD
+        delete foldedParents[parentKey];
       } else { // FOLD
-        foldedParents[anchorKey] = parentNode.getAllChildKeys();
+        foldedParents[parentKey] = childKeys;
       }
       return { foldedParents };
     }, this.forceRender);
